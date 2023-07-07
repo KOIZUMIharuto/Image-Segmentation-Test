a84a0c35006f7a8e604438b914b860feb7557c39
KOIZUMIharuto <105192235+KOIZUMIharuto@users.noreply.github.com>
2023-07-07 17:51:38 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -102,30 +102,30 @@ function enableCam(event) {
   }
 
   // Check if mediaDevices is supported
-  if (navigator.mediaDevices) {
-      // Get available video devices
-      navigator.mediaDevices.enumerateDevices().then((devices) => {
-          let videoDevices = devices.filter(device => device.kind === 'videoinput');
-          let externalCamera = videoDevices.find(device => device.label.toLowerCase().includes('back'));
+  // if (navigator.mediaDevices) {
+  //     // Get available video devices
+  //     navigator.mediaDevices.enumerateDevices().then((devices) => {
+  //         let videoDevices = devices.filter(device => device.kind === 'videoinput');
+  //         let externalCamera = videoDevices.find(device => device.label.toLowerCase().includes('back'));
 
           // getUserMedia parameters.
           const constraints = {
               video: {
-                  deviceId: externalCamera ? { exact: externalCamera.deviceId } : undefined
+                facingMode: "user",
               }
           };
 
-          // Activate the webcam stream.
-          navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-              video.srcObject = stream;
-              video.addEventListener("loadeddata", predictWebcam);
-          });
-      }).catch((error) => {
-          console.error('Error enumerating video devices:', error);
-      });
-  } else {
-      console.warn("navigator.mediaDevices is not supported by your browser");
-  }
+//           // Activate the webcam stream.
+//           navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+//               video.srcObject = stream;
+//               video.addEventListener("loadeddata", predictWebcam);
+//           });
+//       }).catch((error) => {
+//           console.error('Error enumerating video devices:', error);
+//       });
+//   } else {
+//       console.warn("navigator.mediaDevices is not supported by your browser");
+//   }
 }
 
 
