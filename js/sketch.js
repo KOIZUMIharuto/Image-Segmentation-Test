d7e51ba0a438c2fad6fe09650cd22e2eb3bcbe5c
KOIZUMIharuto <105192235+KOIZUMIharuto@users.noreply.github.com>
2023-07-11 12:28:16 +0900

    test

--- js/sketch.js
+++ js/sketch.js
@@ -8,6 +8,7 @@ function setup() {
   // segmentation描画用のグラフィックキャンバス
   pg = createGraphics(100, 100);
 
+  
   // お手々が見つかると以下の関数が呼び出される．resultsに各画素における検出結果（背景が255, selfieが0）が入っている．
   gotSegmentation = function (results) {
     let video = document.querySelector('#webcam');
